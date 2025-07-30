af9373bba84c0d6ec1b7f8b3c922ef96f91fedf1
Tim Liu <timothy@Tims-Mac-mini.local>
2025-07-29 22:43:36 -0400

    Add Cloudinary image display

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import cloudinary
+import cloudinary.api
+import cloudinary.uploader
+
+cloudinary.config(
+  cloud_name='dpr0pl2tf',
+  api_key='548549517251566',
+  api_secret='9o-PlPBRQzQPfuVCQfaGrUV3_IE'
+)
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    # 获取 Cloudinary 中 "photowebsite/" 文件夹下的所有图片
+    resources = cloudinary.api.resources(type="upload", prefix="photowebsite/", max_results=30)
+    image_urls = [img['secure_url'] for img in resources['resources']]
+    return render_template('index.html', image_urls=image_urls)
+
 
 @app.route("/gallery")
 def gallery():
