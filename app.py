b1d7ef702957785a749c2c8cb1bbbc2487ac7885
xia1990 <sweetyx1990@gmail.com>
2025-07-31 17:29:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-import cloudinary
-import cloudinary.api
-import cloudinary.uploader
-
-cloudinary.config(
-  cloud_name='dpr0pl2tf',
-  api_key='548549517251566',
-  api_secret='9o-PlPBRQzQPfuVCQfaGrUV3_IE'
-)
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    image_urls = [
-        "https://res.cloudinary.com/dpr0pl2tf/image/upload/v1753816843/WechatIMG2_mzsnw2.jpg",
-    ]
-    return render_template('index.html', image_urls=image_urls)
+@app.route("/")
+def home():
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
-@app.route("/gallery")
-def gallery():
-    return render_template("gallery.html")
+@app.route("/album")
+def album():
+    return render_template("album.html")
+
+@app.route("/story")
+def story():
+    return render_template("story.html")
 
 if __name__ == "__main__":
     app.run()
