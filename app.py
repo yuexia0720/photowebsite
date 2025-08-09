1c50c7411ca1cb3e883cf34ea35b9dc56bf69f64
xia1990 <sweetyx1990@gmail.com>
2025-08-09 14:19:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, g
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
